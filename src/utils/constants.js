d7deef67b4c0a010e0b55cee110edd11df7e00b5
Garik Asatryan <asa7ur@gmail.com>
2024-05-15 10:00:46 +0200

    Added constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const links = [
+  {
+    id: 1,
+    text: 'inicio',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'productos',
+    url: '/products',
+  },
+  {
+    id: 3,
+    text: 'contacto',
+    url: '/contact',
+  },
+]
